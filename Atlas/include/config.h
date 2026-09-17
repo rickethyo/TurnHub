85beae86624c38b8797d2eb5042f73920eada6ec
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-16 23:25:39 -0400

    Add Atlas prototype configuration

---
+++ Atlas/include/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace AtlasConfig {
+
+constexpr uint8_t MASTER_BUTTON_PIN = 32;
+
+constexpr char WIFI_SSID[] = "TurnHub-Atlas";
+constexpr uint8_t WIFI_CHANNEL = 6;
+
+constexpr uint16_t HTTP_PORT = 80;
+
+}  // namespace AtlasConfig
