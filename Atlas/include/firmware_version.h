deffa67ed7d8d48fd7d79ffa8b9229c08a77b838
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-17 02:13:08 -0400

    Add Atlas firmware version metadata

---
+++ Atlas/include/firmware_version.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace TurnHubFirmware {
+
+constexpr const char *VERSION = "0.1.0-dev";
+constexpr const char *BUILD_DATE = __DATE__;
+constexpr const char *BUILD_TIME = __TIME__;
+
+}  // namespace TurnHubFirmware
