85c81b8efb207f611421aa4606b560cc8f644746
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-18 17:06:02 -0400

    Bump Atlas to 0.4.3-dev for LED pacing

--- Atlas/include/firmware_version.h
+++ Atlas/include/firmware_version.h
@@ -2,7 +2,7 @@
 
 namespace TurnHubFirmware {
 
-constexpr const char *VERSION = "0.4.2-dev";
+constexpr const char *VERSION = "0.4.3-dev";
 constexpr const char *BUILD_DATE = __DATE__;
 constexpr const char *BUILD_TIME = __TIME__;
 
