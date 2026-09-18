8d50a4c66ad339d4f3abc521b7c3da29b2fae74a
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-18 17:45:32 -0400

    Bump unified Sigil firmware to 0.5.1-dev

--- Sigil/include/firmware_version.h
+++ Sigil/include/firmware_version.h
@@ -6,7 +6,7 @@ namespace TurnHubSigilFirmware {
 
 constexpr uint8_t MAJOR = 0;
 constexpr uint8_t MINOR = 5;
-constexpr uint8_t PATCH = 0;
-constexpr const char *VERSION = "0.5.0-dev";
+constexpr uint8_t PATCH = 1;
+constexpr const char *VERSION = "0.5.1-dev";
 
 }  // namespace TurnHubSigilFirmware
