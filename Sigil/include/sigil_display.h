4067d7ec1e9648022e899165b9556f74d07714cb
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-18 20:13:56 -0400

    Let Sigil display render player names

--- Sigil/include/sigil_display.h
+++ Sigil/include/sigil_display.h
@@ -7,8 +7,6 @@
 
 namespace TurnHubSigil {
 
-// Inland / Keyestudio KS0461-compatible 2.13" 250x122 monochrome e-paper.
-// The panel works with GxEPD2's 2.13" B74 / SSD1680 driver.
 class SigilDisplay {
  public:
   SigilDisplay();
@@ -16,6 +14,7 @@ class SigilDisplay {
   void begin();
   void showUnpaired();
   void showReady(uint8_t sigilId);
+  void setSeatName(uint8_t slot, const char *name);
   void showState(
       uint8_t sigilId,
       TurnHubProtocol::DisplayMode mode,
@@ -28,11 +27,15 @@ class SigilDisplay {
   void drawHeader(const char *title);
   void drawStatus(const char *line1, const char *line2 = nullptr);
   void drawPlayerLabel(uint8_t primaryPlayer, uint8_t secondaryPlayer);
+  void printClipped(const char *text, uint8_t maxChars);
 
   static constexpr int8_t EPD_CS = 17;
   static constexpr int8_t EPD_DC = 16;
   static constexpr int8_t EPD_RST = 22;
   static constexpr int8_t EPD_BUSY = 21;
+
+  char seatNameA_[TurnHubProtocol::DISPLAY_NAME_MAX_LENGTH + 1] = {};
+  char seatNameB_[TurnHubProtocol::DISPLAY_NAME_MAX_LENGTH + 1] = {};
 
   GxEPD2_BW<GxEPD2_213_B74, GxEPD2_213_B74::HEIGHT> display_;
 };
