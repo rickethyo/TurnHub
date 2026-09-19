362989fd11351c63eb41ba3cbebc8268c16e870f
Ricky Buchanan <ricketh.yo@gmail.com>
2026-09-18 20:50:51 -0400

    Correct Sigil display profile version metadata

--- Sigil/include/firmware_version.h
+++ Sigil/include/firmware_version.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <Arduino.h>
+
 namespace TurnHubSigilFirmware {
 
 constexpr uint8_t MAJOR = 0;
 constexpr uint8_t MINOR = 5;
-constexpr uint8_t PATCH = 1;
-constexpr const char *VERSION = "0.5.1-dev";
+constexpr uint8_t PATCH = 3;
+constexpr const char *VERSION = "0.5.3-dev";
 
 }  // namespace TurnHubSigilFirmware
